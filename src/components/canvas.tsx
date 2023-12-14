1214c0a6ae00458e4d08275c5b030dcdfa362102
Dagadev <gacub20ru@gmail.com>
2023-12-13 22:51:31 -0600

    add generate random piece function & add checkCollision function & add remove row & add rotate piece function

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,22 +1,27 @@
 import { useState, useEffect } from 'react'
-import { SHAPES } from '../utils/consts'
-
-const BLOCK_SIZE = 20
-const BOARD_WIDTH = 16
-const BOARD_HEIGHT = 18
+import { BLOCK_SIZE, BOARD_WIDTH, BOARD_HEIGHT, SHAPES } from '../utils/consts'
 
 console.log('root')
 
 const board: number[][] = []
-const randomColum = Math.floor(Math.random() * BOARD_WIDTH)
 
 const piece = {
   position: {
     y: 0,
-    x: randomColum,
+    x: 0
   },
-  shape: SHAPES[Math.floor(Math.random() * SHAPES.length)]
+  shape: SHAPES[0]
 }
+
+const generateRandomPiece = () => {
+  const randomShape = SHAPES[Math.floor(Math.random() * SHAPES.length)]
+  const randomColum = Math.floor(Math.random() * (BOARD_WIDTH - randomShape[0].length + 1))
+
+  piece.position.y = 0
+  piece.position.x = randomColum
+  piece.shape = randomShape
+}
+generateRandomPiece()
 
 console.log(piece)
 
@@ -30,66 +35,6 @@ for (let y=0; y<BOARD_HEIGHT; y++) {
   board.push(row)
 }
 
-// piece.shape.forEach((row, y) => {
-//   row.forEach((value, x) => {
-//     if (value === 2) {
-//       if (y == 0 && x == 0) piece.position.x = randomColum-row.length+x
-//       board[y].splice(randomColum-row.length+x, 1, value)
-//     }
-//   })
-// })
-// board[2].splice(1, 1, 2)
-// board[2].splice(2, 2, ...[1, 1])
-// board[3].splice(2, 2, ...[1, 1])
-
-function draw (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
-  // console.log(piece.position)
-  context.fillStyle = '#000'
-  context.fillRect(0, 0, canvas.width, canvas.height)
-
-  board.forEach((row, y) => {
-    row.forEach((value, x) => {
-      if (value === 1) {
-        context.fillStyle = 'orange'
-        context.fillRect(x, y, 1, 1)
-      }
-
-      if (value === 2) {
-        context.fillStyle = 'yellow'
-        context.fillRect(x, y, 1, 1)
-      }
-    })
-  })
-
-  piece.shape.forEach((row, y) => {
-    row.forEach((value, x) => {
-      if (value === 2) {
-        if (y == 0 && x == 0) piece.position.x = randomColum-row.length+x
-        x = randomColum+x > BOARD_WIDTH-1 ? randomColum + x - piece.shape[0].length : randomColum+x
-        piece.position.x = x
-        context.fillStyle = 'yellow'
-        context.fillRect(x, y, 1, 1)
-      }
-    })
-  })
-}
-
-function movePice(x: number, y: number) {
-  const newX = x < 0 ? piece.position.x - x : piece.position.x + piece.shape[0].length + x
-  const newY = y < 0 ? piece.position.y - y : piece.position.y + piece.shape.length + y
-  
-  if (newX >= 0 && newX <= BOARD_WIDTH) {
-    piece.shape.forEach((row, y) => {
-      row.forEach((value, x) => {
-        if (value === 2) {
-          if (y == 0 && x == 0) piece.position.x = newX-row.length+x
-          board[y].splice(newX-row.length+x, 1, value)
-        }
-      })
-    })
-  }
-}
-
 export default function Canvas () {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
 
@@ -101,47 +46,149 @@ export default function Canvas () {
       canvas.height = BOARD_HEIGHT * BLOCK_SIZE
     
       context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+      const draw = () => {
+        context.fillStyle = '#000'
+        context.fillRect(0, 0, canvas.width, canvas.height)
       
+        board.forEach((row, y) => {
+          row.forEach((value, x) => {
+            if (value) {
+              context.fillStyle = 'orange'
+              context.fillRect(x, y, 1, 1)
+            }
+          })
+        })
+      
+        piece.shape.forEach((row, y) => {
+          row.forEach((value, x) => {
+            const finalY = piece.position.y + y
+            if (value) {
+              x = piece.position.x + x
+
+              context.fillStyle = 'yellow'
+              context.fillRect(x, finalY, 1, 1)
+            }
+          })
+        })
+      }
+
+      const checkCollision = (y: number, x: number) => {
+        return piece.shape.some((row, rowI) => {
+          return row.some((value, columI) => {
+            if (value) {
+              const boardElement = board[rowI+y]?.[columI+x]
+              return boardElement === undefined || boardElement === 1
+            } else return false
+          })
+        })
+      }
+
+      const handleRemoveRow = () => {
+        if (board.some(r => r.every(c => c))) {
+          console.log('remove row')
+          board.splice(board.findIndex(r => r.every(c => c)), 1)
+          board.unshift(board[0])
+        }
+      }
+
+      const rotatePiece = () => {
+        const rotatedShape: number[][] = []
+        const reversedShape = piece.shape.reverse()
+
+        for (let y=0; y<piece.shape.length; y++) {
+          const row = reversedShape[y]
+
+          for (let x=0; x<row.length; x++) {
+            const value = row[x]
+
+            if (value) {
+              const boardElement = board[piece.position.y+x]?.[piece.position.x+y]
+              if (boardElement === undefined || boardElement === 1) {
+                return
+              }
+            }
+            
+            if (!rotatedShape[x]) rotatedShape.push([value])
+            else rotatedShape[x][y] = value
+          }
+        }
+
+        piece.shape = rotatedShape
+      }
+
+      const solidifyPiece = () => {
+        const { y, x } = piece.position
+        piece.shape.forEach((row, rowI) => {
+          row.forEach((value, columI) => {
+            if (value) board[y+rowI][x+columI] = 1
+          })
+        })
+
+
+        handleRemoveRow()
+        generateRandomPiece()
+      }
+
       const update = () => {
-        draw(canvas, context)
+        draw()
         // window.requestAnimationFrame(update)
       }
 
-      const handleKeyUp = ({ key }: KeyboardEvent) => {
-        console.log(key)
+      const handleKeydown = ({ key }: KeyboardEvent) => {
+        const { y, x } = piece.position
+        // console.log(key)
+
+        if (['w', 'ArrowUp'].some(s=> s == key)) {
+          rotatePiece()
+        }
 
         if (['s', 'ArrowDown'].some(s=> s == key)) {
-          piece.position.y++
-          console.log(piece.position)
+          if (checkCollision(y+1, x)) {
+            solidifyPiece()
+          } else {
+            piece.position.y++
+          }
         }
         
         if (['d', 'ArrowRight'].some(s=> s == key)) {
-          piece.position.x++
+          if (!checkCollision(y, x+1)) {
+            piece.position.x++
+          }
         }
         
         if (['a', 'ArrowLeft'].some(s=> s == key)) {
-          piece.position.x--
+          if (!checkCollision(y, x-1)) {
+            piece.position.x--
+          }
         }
       }
     
       update()
-      document.addEventListener('keyup', handleKeyUp)
+      document.addEventListener('keydown', handleKeydown)
       
-      // let time = 0
+      let time = 0
 
       const interval = setInterval(() => {
-        update()
+        const { y, x } = piece.position
 
         // time++
-        // if (time === 10) {
-        //   piece.position.y++
-        //   time=0
-        // }
+        if (time === 10) {          
+          if (checkCollision(y+1, x)) {
+            solidifyPiece()
+          } else {
+            piece.position.y++
+          }
+          time=0
+        }
+        
+        update()
+        
       }, 100)
   
       return () => {
         clearInterval(interval)
-        document.removeEventListener('keyup', handleKeyUp)
+        document.removeEventListener('keyup', handleKeydown)
       }
     }
   }, [canvas])
