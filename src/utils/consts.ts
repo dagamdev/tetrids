99b1db8de580df92474ec2e22e4190e64fe01fda
Dagadev <gacub20ru@gmail.com>
2024-02-13 11:09:17 -0600

    Update block size & Update board dimentions

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,6 +1,6 @@
-export const BLOCK_SIZE = 20
-export const BOARD_WIDTH = 16
-export const BOARD_HEIGHT = 18
+export const BLOCK_SIZE = 30
+export const BOARD_WIDTH = 14
+export const BOARD_HEIGHT = 16
 
 export const SHAPES = [
   [
